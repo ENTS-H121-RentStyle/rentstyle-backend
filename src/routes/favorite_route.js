84c63746899b73c954b200a3c9a971077ca4ab34
idanpostman <idanpostman@gmail.com>
2024-05-30 14:26:54 +0700

    Validasi Favorit (Done)

--- src/routes/favorite_route.js
+++ src/routes/favorite_route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import tokenAuth from "../middleware/auth.js";
 import Controller from "../controllers/favorite_controller.js";
+import validateAddFavorite from "../middleware/validate_add_favorite.js";
 
 
 const router = Router();
 
 router
-    .post("/", tokenAuth, Controller.addFavorite)
+    .post("/", tokenAuth, validateAddFavorite, Controller.addFavorite)
     .get("/", Controller.getAllFavorite)
     .get("/search", Controller.getSearchFavorite)
     .get("/:id", Controller.findUserFavorite)
