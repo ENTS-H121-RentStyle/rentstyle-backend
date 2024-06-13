dc877d4154f63b8a0d97d6489a79bb2ef41fbd56
idanpostman <idanpostman@gmail.com>
2024-06-13 20:27:29 +0700

    Benerin router izinAuth

--- src/routes/result_route.js
+++ src/routes/result_route.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import Controller from "../controllers/result_controller.js";
 import validateAddResult from "../middleware/validate_add_result.js";
-import izin from "../middleware/izin.js";
+import izinAuth from "../middleware/izin.js";
 
 
 const router = Router();
 
 router
-    .post("/model1", izin, validateAddResult, Controller.addResultModel1)
-    .post("/model2", izin, validateAddResult, Controller.addResultModel2)
-    .get("/model1", Controller.getResultModel1)
-    .get("/model2", Controller.getResultModel2);
+    .post("/model1", izinAuth, validateAddResult, Controller.addResultModel1)
+    .post("/model2", izinAuth, validateAddResult, Controller.addResultModel2)
+    .get("/model1", izinAuth, Controller.getResultModel1)
+    .get("/model2", izinAuth, Controller.getResultModel2);
 
 export default router;
