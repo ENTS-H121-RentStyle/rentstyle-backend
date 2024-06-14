6964587e41adf6545305b71a3e9b8d9f95250cc2
Dhyoga <dhyogap@gmail.com>
2024-06-14 07:30:42 +0700

    Revisi Data Interaction

--- src/controllers/review_controller.js
+++ src/controllers/review_controller.js
@@ -1,87 +1,103 @@
 import { ReviewService } from "../services/review_service.js";
 import { validationResult } from "express-validator";
 import { Review } from "../models/review_model.js";
-import { uploadFileToGCS, deleteFileFromGCS } from "../services/image_service.js";
+import {
+  uploadFileToGCS,
+  deleteFileFromGCS,
+} from "../services/image_service.js";
 
 const service = new ReviewService();
 
 const addReview = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    
-    try {
-        const { order_id, product_id } = req.body;
-        const existingReview = await Review.findOne({
-            where: {
-                order_id: order_id,
-                product_id: product_id,
-            },
-        });
-        if (existingReview) {
-            return res.status(400).json({ message: "User sudah melakukan review pada order ini" });
-        }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-        let imageUrl = null;
-        if (req.file) {
-            imageUrl = await uploadFileToGCS(req.file, "review");
-        }
-    
-        const reviewData = {
-            ...req.body,
-            image: imageUrl,
-        };
-    
-        const response = await service.create(reviewData);
-        res.status(201).json(response);
-    } catch (error) {
-        res.status(500).send({ message: error.message });
+  try {
+    const { order_id, product_id } = req.body;
+    const existingReview = await Review.findOne({
+      where: {
+        order_id: order_id,
+        product_id: product_id,
+      },
+    });
+    if (existingReview) {
+      return res
+        .status(400)
+        .json({ message: "User sudah melakukan review pada order ini" });
     }
-}
 
-const getReviewByProductId = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const response = await service.readReviewByProductId(id);
-
-        if (response.length === 0) {
-            return res.status(404).json({ message: "Review Product tidak ditemukan" });
-        }
-    
-        const sanitizedResponse = response.map((item) => {
-            const { product_id, user_id, ...rest } = item.toJSON();
-            return rest;
-        });
-        res.status(200).json(sanitizedResponse);
-    } catch (error) {
-        res.status(500).send({ message: error.message });
+    let imageUrl = null;
+    if (req.file) {
+      imageUrl = await uploadFileToGCS(req.file, "review");
     }
+
+    const reviewData = {
+      ...req.body,
+      image: imageUrl,
+    };
+
+    const response = await service.create(reviewData);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 };
 
-const dropReview = async(req, res) => {
-    try {
-        const { id } = req.params;
-        const review = await Review.findByPk(id);
+const getAllReview = async (req, res) => {
+  try {
+    const response = await service.readAll();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
 
-        if (!review) {
-            return res.status(404).json({ message: "Review tidak ditemukan!" });
-        }
+const getReviewByProductId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await service.readReviewByProductId(id);
 
-        if (review.image) {
-            await deleteFileFromGCS(review.image);
-        }
-
-        const response = await service.delete(id);
-        res.status(200).json(response);
-
-    } catch (error) {
-        res.status(500).send({ message: error.message });
+    if (response.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Review Product tidak ditemukan" });
     }
-}
+
+    const sanitizedResponse = response.map((item) => {
+      const { product_id, user_id, ...rest } = item.toJSON();
+      return rest;
+    });
+    res.status(200).json(sanitizedResponse);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+const dropReview = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const review = await Review.findByPk(id);
+
+    if (!review) {
+      return res.status(404).json({ message: "Review tidak ditemukan!" });
+    }
+
+    if (review.image) {
+      await deleteFileFromGCS(review.image);
+    }
+
+    const response = await service.delete(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
 
 export default {
-    addReview,
-    getReviewByProductId,
-    dropReview
-}
+  addReview,
+  getAllReview,
+  getReviewByProductId,
+  dropReview,
+};
--- src/routes/review_route.js
+++ src/routes/review_route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import tokenAuth from "../middleware/auth.js";
+import izinAuth from "../middleware/izin.js";
 import Controller from "../controllers/review_controller.js";
 import validateAddReview from "../middleware/validate_add_review.js";
 import { uploadMiddleware } from "../services/image_service.js";
@@ -7,8 +8,9 @@ import { uploadMiddleware } from "../services/image_service.js";
 const router = Router();
 
 router
-    .post("/", uploadMiddleware, validateAddReview, Controller.addReview)
-    .get("/:id", Controller.getReviewByProductId)
-    .delete("/:id", tokenAuth, Controller.dropReview);
+  .post("/", uploadMiddleware, validateAddReview, Controller.addReview)
+  .get("/", izinAuth, Controller.getAllReview)
+  .get("/:id", Controller.getReviewByProductId)
+  .delete("/:id", tokenAuth, Controller.dropReview);
 
 export default router;
