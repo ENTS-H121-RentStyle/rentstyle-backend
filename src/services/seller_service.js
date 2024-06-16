846ce767ad0ab8d5e68f4b2752c621596591e5c2
Dhyoga <dhyogap@gmail.com>
2024-06-16 16:18:06 +0700

    Finalisasi

--- src/services/seller_service.js
+++ src/services/seller_service.js
@@ -3,33 +3,41 @@ import crypto from "crypto";
 import { Seller } from "../models/seller_model.js";
 
 class SellerService {
-    constructor() {};
+  constructor() {}
 
-    async create(data) {
-        const sellerId = crypto.randomUUID();
-        const res = await Seller.create({ ...data, id: sellerId });
-        return res;
+  async create(data) {
+    const sellerId = crypto.randomUUID();
+    const res = await Seller.create({ ...data, id: sellerId });
+    return res;
+  }
+
+  async readOne(keyId) {
+    const res = await Seller.findOne({
+      where: {
+        [Op.or]: [
+          { id: keyId },
+          {
+            user_id: keyId,
+          },
+        ],
+      },
+    });
+    return res;
+  }
+
+  async update(id, data) {
+    const model = await Seller.findByPk(id);
+    const res = await model.update(data);
+    return res;
+  }
+
+  async delete(id) {
+    const model = await Seller.findByPk(id);
+    await model.destroy();
+    return {
+      deleted: true,
     };
-
-    async readOne(sellerId) {
-        const res = await Seller.findByPk(sellerId);
-        return res;
-    };
-
-
-    async update(id, data) {
-        const model = await Seller.findByPk(id);
-        const res = await model.update(data);
-        return res;
-    };
-
-    async delete(id) {
-        const model = await Seller.findByPk(id);
-        await model.destroy();
-        return {
-            deleted: true
-        };
-    };
-};
+  }
+}
 
 export { SellerService };
