529a4beaca92439cb164ad0e10fa4dbbd324da3d
Dhyoga <dhyogap@gmail.com>
2024-06-16 14:04:38 +0700

    Alternative Explore Seller

--- src/services/explore_service.js
+++ src/services/explore_service.js
@@ -38,6 +38,36 @@ class ExploreService {
 
     return res;
   }
+
+  async readImage() {
+    const res = await Seller.findAll({
+      attributes: [
+        "id",
+        "seller_name",
+        "image",
+        "city",
+        [Sequelize.fn("AVG", Sequelize.col("Reviews.rating")), "avg_rating"],
+        // [
+        //   Sequelize.fn("COUNT", Sequelize.col("Products.product_id")),
+        //   "count_num_product",
+        // ],
+      ],
+      include: [
+        {
+          model: Product,
+          limit: 3,
+          attributes: ["id", "image"],
+        },
+        {
+          model: Review,
+          attributes: [], // Review attributes jika diperlukan
+        },
+      ],
+      group: ["Seller.seller_id"], // Group by Seller id (perhatikan penggunaan Seller.id bukan Seller.seller_id)
+      order: [[Sequelize.literal("avg_rating"), "DESC"]],
+    });
+    return res;
+  }
 }
 
 export { ExploreService };
